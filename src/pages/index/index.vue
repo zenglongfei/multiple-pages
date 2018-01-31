c1d9af6a38e38e37b8cc117c0cf037d6983f1c92
曾龙飞 <511698296@qq.com>
2018-01-31 22:46:18 +0800

    loading、弹框、axios、store、router

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,26 @@
 <template>
-  <h1 @click="goLogin">这是主页</h1>
+  <div>
+    <loading-component/>
+    <h1 @click="goLogin">这是主页</h1>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'index',
-    methods: {
-      goLogin () {
-        location.assign('login.html')
-      }
+import LoadingComponent from '../../components/loadingComponent'
+
+export default {
+  components: {LoadingComponent},
+  name: 'index',
+  methods: {
+    goLogin () {
+      location.assign('login.html')
     }
   }
+}
 </script>
 
 <style scoped>
-
+html {
+  font-size: 18px;
+}
 </style>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>这是登陆页面</h1>
+  <h1 @click="goHome">这是登陆页面</h1>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  methods: {
+    goHome () {
+      location.assign('index.html')
+    }
+  }
 }
 </script>
 
